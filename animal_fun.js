b1ad6ad35148611dba69cac6ce1d909656b98155
Tony Xie <tony_xie@outlook.com>
2017-09-20 16:46:12 -0700

    Initial Commit

---
+++ animal_fun.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// })
+
+
+// fs.writeFile('./example.txt', 'I will be written to example.txt', err => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("file successfully written!");
+//   }
+// })
+
+console.log(process.argv[2])
