68c6847a4235c4d37c92f5eb5220b1c7bb9fffbb
Tony Xie <tony_xie@outlook.com>
2017-09-21 22:43:33 -0700

    part1: function extracts animals starting with letter inputted to its own text file

--- animal_fun.js
+++ animal_fun.js
@@ -17,4 +17,32 @@ const fs = require('fs');
 //   }
 // })
 
-console.log(process.argv[2])
+// console.log(process.argv[2])
+
+var letter = process.argv[2];
+var animals = new Array;
+
+fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+  if (err){
+    console.log(err);
+  } else {
+    animals = (data.split('\n'));
+  }
+
+  var matchedAnimals = [];
+  animals.forEach(function(animal) {
+    if (animal[0] && animal[0].toUpperCase() === letter.toUpperCase()) {
+      matchedAnimals.push(animal)
+    }
+  })
+  // console.log(matchedAnimals)
+
+  fs.writeFile(`./${letter}_animals.txt`, matchedAnimals, err => {
+    if (err){
+      console.log(err);
+    } else {
+      console.log("file successfully written!");
+    }
+  });
+
+})
